3adb113aec2488b6bfe0ca9fae900ffee28d2eaa
IBST-yoonSangHyeon <47581425+IBST-yoonSangHyeon@users.noreply.github.com>
2021-08-23 17:23:56 +0900

    make customers

--- src/App.js
+++ src/App.js
@@ -2,22 +2,61 @@ import React, { Component } from 'react';
 import './app.css';
 import Customer from './components/customer';
 
-const customer = {
-  name: '홍길동',
-  birthday: '961222',
-  gender: '남자',
-  job : '학생',
-}
+// const customer = {
+//   id: 1,
+//   image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/1200px-Flat_tick_icon.svg.png',
+//   name: '홍길동',
+//   birthday: '961222',
+//   gender: '남자',
+//   job : '학생',
+// }
+
+const customer = [
+  {
+    id: 1,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/1200px-Flat_tick_icon.svg.png',
+    name: '홍길동',
+    birthday: '961222',
+    gender: '남자',
+    job : '학생',
+  },
+  {
+    id: 2,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/1200px-Flat_tick_icon.svg.png',
+    name: '임윤아',
+    birthday: '961222',
+    gender: '여자',
+    job : '가수',
+  },
+  {
+    id: 3,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/1200px-Flat_tick_icon.svg.png',
+    name: '마크로',
+    birthday: '961222',
+    gender: '남자',
+    job : '텔런트',
+  },
+];
 
 class  App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday }
-        gender={customer.gender }
-        job={customer.job }
-      />
+      <div>
+        {
+          customer.map((customer) => {
+            return <Customer
+                    id={customer.id}
+                    image={customer.image}
+                    name={customer.name}
+                    birthday={customer.birthday }
+                    gender={customer.gender }
+                    job={customer.job }
+                  />
+          })
+        }
+      
+      
+      </div>
     );
 }
 }
--- src/components/customer.jsx
+++ src/components/customer.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
+
+
+
 class Customer extends Component {
     render() {
         return (
             <div>
-                <h2>{ this.props.name}</h2>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        );
+    }
+}
+
+class CustomerProfile extends Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile" width="60"/>
+                <h1>{this.props.name}({this.props.id})</h1>
+            </div>
+        );
+    }
+}
+
+class CustomerInfo extends Component {
+    render() {
+        return (
+            <div>
                 <p>{ this.props.birthday}</p>
                 <p>{ this.props.gender}</p>
                 <p>{ this.props.job}</p>
@@ -12,5 +36,8 @@ class Customer extends Component {
         );
     }
 }
+
+
+
 
 export default Customer;
